36a87032cc93821092e3b14d01cb8fc9ca97b8b7
Lee Changmoo <lcm8543@gmail.com>
2022-02-14 14:40:25 +0900

    Add other Toggle files at the Toggle folder

--- design-system/src/features/Toggle/ToggleDemo.js
+++ design-system/src/features/Toggle/ToggleDemo.js
@@ -1,13 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 import { FeatureContainer } from "../layout/layout";
 import { Feature } from "../layout/layout";
-import Toggle from "./Toggle";
+import FilledToggle from "./FilledToggle";
+import BasicToggle from "./BasicToggle";
+import SquareToggle from "./SquareToggle";
+
+const Toggles = styled.div`
+  width: 500px;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
 
 const ToggleDemo = () => {
+  let controlMode = "none";
+  /* If you want to make the "disabled Toggle", 
+  please change the variable => controlMode = "disabled" */
+  /* If you want to make the "default checked Toggle", 
+   please change the variable => controlMode = "defaultChecked" */
+
   return (
     <FeatureContainer>
       <Feature>
-        <Toggle />
+        <Toggles>
+          <BasicToggle controlMode={controlMode} />
+          <SquareToggle controlMode={controlMode} />
+          <FilledToggle controlMode={controlMode} />
+        </Toggles>
       </Feature>
     </FeatureContainer>
   );
